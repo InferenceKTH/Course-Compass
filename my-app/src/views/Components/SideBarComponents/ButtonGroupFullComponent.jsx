e25b175f85d9732d974942d4c3bc2fd0bc8ebb20
Sami Al Saati <sami.alsaati@hotmail.com>
2025-05-08 11:21:22 +0200

    Update ButtonGroupFullComponent.jsx

--- my-app/src/views/Components/SideBarComponents/ButtonGroupFullComponent.jsx
+++ my-app/src/views/Components/SideBarComponents/ButtonGroupFullComponent.jsx
@@ -5,8 +5,8 @@ import Tooltip from "./ToolTip";
 
 export default function ButtonGroupFullComponent(props) {
     const [filterEnabled, setFilterEnabled] = useState(props.filterEnable);
-    const [selectedItems, setSelectedItems] = useState(props.initialValues);
-
+    const [selectedItems, setSelectedItems] = useState(props.initialValues || []);
+    
     const handleClick = (index) => {
         const selectedItem = props.items[index];
         setSelectedItems((prevSelectedItems) => {
@@ -19,13 +19,24 @@ export default function ButtonGroupFullComponent(props) {
 
     const getButtonClasses = (index) => {
         if (!selectedItems || !Array.isArray(selectedItems)) {
-            return "default-button-class";
-        }
-        if (index < 0 || index >= selectedItems.length) {
-            return "default-button-class";
+            return "default-button-class"; // Fallback class if selectedItems is invalid
         }
 
-        return selectedItems[index] ? "selected-class" : "unselected-class";
+        if (index < 0 || index >= selectedItems.length) {
+            return "default-button-class"; // Fallback class for invalid index
+        }
+
+        const baseClasses = `flex-auto py-1 px-4 inline-flex items-center gap-x-2 text-sm
+      font-medium focus:z-10 border border-gray-200 shadow-2xs hover:bg-[#8785ac]
+      focus:outline-hidden disabled:opacity-50 disabled:pointer-events-none pl-8`;
+        const activeClass = selectedItems[index] ? "bg-violet-500" : "bg-transparent";
+        const roundedClasses =
+            index === 0
+                ? "rounded-l-lg"
+                : index === props.items.length - 1
+                    ? "rounded-r-lg"
+                    : "border-l-0";
+        return `${baseClasses} ${activeClass} ${roundedClasses}`;
     };
 
     return (
